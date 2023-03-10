96991b4f3be92d006e5fe3d279b4d450cce7c606
Taylor <tcurran@ualberta.ca>
2023-03-10 12:35:54 -0700

    please build and fail test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.lab8.R;
+import com.example.lab88.R;
 
 import java.util.ArrayList;
 
