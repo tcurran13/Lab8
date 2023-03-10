72a18adfa8f367988ee709523836c178dfe88f84
Taylor <tcurran@ualberta.ca>
2023-03-09 23:15:52 -0700

    TEST addCity()
    wrote the test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+
+    private CustomList list;
+    /**
+     * create a mocklist for cityList
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus
+     * one
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Red Deer", "AB"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 
 
 }
