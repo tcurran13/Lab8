f9451bb64a8f419e2956cd78667fe94453bfc39d
Taylor <tcurran@ualberta.ca>
2023-03-10 01:16:13 -0700

    test fail

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -62,6 +62,6 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public void addCity(City city){
-        cities.add(city);
+        //cities.add(city);
     }
 }
